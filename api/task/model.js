44b95fc3224460f15a01e36b8356e6639c250c3e
LBU <lbu@LBUui-MacBook-Pro.local>
2020-12-11 14:43:04 -0500

    debugging...

--- api/task/model.js
+++ api/task/model.js
@@ -9,6 +9,7 @@ module.exports = {
         return db('tasks')
         .insert(description)
         .join('projects', 'tasks.projects_id', 'projects.id')
+        .select('projects.name', 'projects.description','tasks.id', 'tasks.description')
         .where('tasks.projects_id', id)
     }
 }
